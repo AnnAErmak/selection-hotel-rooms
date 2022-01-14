49031faccfc255a1dc25af4d0b2d13f97f409f06
Ermak_AnnA <ermak-anna86@yandex.ru>
2022-01-14 10:39:31 +0300

    Added fonts, global styles and button form element with background

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import './styles/index.scss'
-const userStatsk = {
-    language: 'JavaScript',
-    framework: 'React'
-}
-
-const user = {
-    name: 'Anna',
-    age: '37',
-    ...userStatsk
-}
-console.log(user)
+import './style/index.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let mode = 'development'
 if(process.env.NODE_ENV === 'production') {
     mode = 'production'
 }
-console.log(mode + ' mode')
+
 module.exports = {
     mode: mode,
     output: {
